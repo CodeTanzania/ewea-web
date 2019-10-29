6bd11fd75bbdaa1d7172fce99134b9391c82d03c
Benson Maruchu <benmaruchu@gmail.com>
2019-10-29 19:32:24 +0300

    fix(alert dashboard): improve ui for zoom controls

--- src/Dashboards/index.js
+++ src/Dashboards/index.js
@@ -32,31 +32,13 @@ const BASE_COLOR = '#ff0000';
 const ZoomControl = ({ handleZoomIn, handleZoomOut }) => {
   return (
     <div className="ZoomControl">
-      <Button onClick={handleZoomIn}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          strokeWidth="3"
-        >
-          <line x1="12" y1="5" x2="12" y2="19" />
-          <line x1="5" y1="12" x2="19" y2="12" />
-        </svg>
-      </Button>
-      <Button onClick={handleZoomOut}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          strokeWidth="3"
-        >
-          <line x1="5" y1="12" x2="19" y2="12" />
-        </svg>
-      </Button>
+      <Button type="default" shape="round" icon="plus" onClick={handleZoomIn} />
+      <Button
+        type="default"
+        shape="round"
+        icon="minus"
+        onClick={handleZoomOut}
+      />
     </div>
   );
 };
