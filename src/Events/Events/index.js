64bd3d761633a3863cd140bde21633875bd6ef00
Benson Maruchu <benmaruchu@gmail.com>
2020-04-15 11:38:27 +0300

    improvement(events): add time elapsed since last update

--- src/Events/Events/index.js
+++ src/Events/Events/index.js
@@ -30,6 +30,7 @@ import {
   notifySuccess,
   truncateString,
   generateEventTemplate,
+  timeAgo,
 } from '../../util';
 import './styles.css';
 
@@ -43,12 +44,13 @@ const {
   getEventsExportUrl,
 } = httpActions;
 const { confirm } = Modal;
-const referenceIDSpan = { xxl: 4, xl: 4, lg: 4, md: 5, sm: 8, xs: 8 };
+const eventSpan = { xxl: 6, xl: 7, lg: 7, md: 7, sm: 12, xs: 12 };
+const referenceIDSpan = { xxl: 3, xl: 3, lg: 3, md: 5, sm: 8, xs: 8 };
 const typeSpan = { xxl: 3, xl: 3, lg: 4, md: 3, sm: 0, xs: 0 };
-const stageSpan = { xxl: 3, xl: 3, lg: 2, md: 3, sm: 0, xs: 0 };
-const levelSpan = { xxl: 3, xl: 3, lg: 2, md: 3, sm: 0, xs: 0 };
-const groupSpan = { xxl: 3, xl: 3, lg: 4, md: 0, sm: 0, xs: 0 };
-const eventSpan = { xxl: 6, xl: 6, lg: 7, md: 7, sm: 12, xs: 12 };
+const stageSpan = { xxl: 2, xl: 2, lg: 2, md: 2, sm: 0, xs: 0 };
+const levelSpan = { xxl: 3, xl: 2, lg: 2, md: 3, sm: 0, xs: 0 };
+const groupSpan = { xxl: 2, xl: 2, lg: 4, md: 0, sm: 0, xs: 0 };
+const lastUpdatedSpan = { xxl: 2, xl: 2, lg: 1, md: 1, sm: 0, xs: 0 };
 
 const headerLayout = [
   { ...eventSpan, header: 'Event' },
@@ -57,6 +59,7 @@ const headerLayout = [
   { ...stageSpan, header: 'Event Stage' },
   { ...typeSpan, header: 'Event Type' },
   { ...groupSpan, header: 'Event Group' },
+  { ...lastUpdatedSpan, header: 'Last Updated' },
   // { ...urgencySpan, header: 'Urgency' },
 ];
 
@@ -510,6 +513,7 @@ class Events extends Component {
               <Col {...groupSpan}>
                 {get(item, 'group.strings.name.en', 'N/A')}
               </Col>
+              <Col {...lastUpdatedSpan}>{timeAgo(item.updatedAt)}</Col>
               {/* eslint-enable react/jsx-props-no-spreading */}
             </ListItem>
           )}
