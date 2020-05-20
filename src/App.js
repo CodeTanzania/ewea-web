dcd370e9c55b20b6e0b725e3f4818130ac5711b2
Benson Maruchu <benmaruchu@gmail.com>
2020-05-20 09:48:21 +0300

    chore(env): show banner on set env variable

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import React from 'react';
 import { HashRouter, Switch, Route, Redirect } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 import ReactGA from 'react-ga';
+import get from 'lodash/get';
 
 import SignIn from './Auth/components/SignIn';
 import EventFeedback from './Events/EventFeedback';
@@ -16,6 +17,7 @@ import SecureRoute from './Auth/SecureRoute';
 Spin.setDefaultIndicator(<LoadingOutlined style={{ fontSize: 24 }} spin />);
 
 const history = createBrowserHistory();
+const showBanner = JSON.parse(get(process.env, 'REACT_APP_SHOW_BANNER', false));
 
 history.listen((location) => {
   ReactGA.pageview(location.hash);
@@ -39,12 +41,14 @@ const App = () => {
 
   return (
     <StoreProvider>
-      <Alert
-        message="This is a depiction of the Emergency Management Information System for the sole purpose of development and training of users. The system itself is located exclusively on government servers at DarMAERT EOC, and collected data is not accessible to any external parties"
-        type="error"
-        banner
-        showIcon={false}
-      />
+      {showBanner && (
+        <Alert
+          message="This is a depiction of the Emergency Management Information System for the sole purpose of development and training of users. The system itself is located exclusively on government servers at DarMAERT EOC, and collected data is not accessible to any external parties"
+          type="error"
+          banner
+          showIcon={false}
+        />
+      )}
       <HashRouter hashType="hashbang" history={history}>
         <Switch>
           <SecureRoute path="/app" component={BaseLayout} />
