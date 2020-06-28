074d88ed1bf85815d8a5ddd5ac3b13a119722327
Benson Maruchu <benmaruchu@gmail.com>
2020-06-28 19:52:55 +0300

    docs(access-control): update permit component jsdoc

--- src/components/Permit/index.js
+++ src/components/Permit/index.js
@@ -1,6 +1,25 @@
 import PropTypes from 'prop-types';
 import { Connect } from '@codetanzania/ewea-api-states';
 
+/**
+ * @function
+ * @name Permit
+ * @description Component which controls visibility of child components based on
+ * user permissions
+ * @param {object} props Component object properties
+ * @param {string} props.withPermission Permission wildcard which controls
+ * visibility of children components
+ * @param {string[]} props.userPermissions List of active user permissions
+ * @param {object|object[]} props.children Wrapped Components (React Nodes)
+ * @returns {object|undefined} React nodes
+ * @version 0.1.0
+ * @since 0.1.0
+ * @example
+ *
+ * <Permit withPermission="events:list">
+ *   {...children}
+ * </Permit>
+ */
 const Permit = ({ withPermission, userPermissions, children }) => {
   return userPermissions.includes(withPermission) && children;
 };
