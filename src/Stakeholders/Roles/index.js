04413dc274429c061daf006aefd1f410a373ea9b
Benson Maruchu <benmaruchu@gmail.com>
2020-10-12 14:23:09 +0300

    fix(party-roles): update method params on jsdoc

--- src/Stakeholders/Roles/index.js
+++ src/Stakeholders/Roles/index.js
@@ -109,10 +109,9 @@ const Roles = ({
 
   /**
    * @function
-   * @param item
    * @name handleOnAssignPermissions
    * @description Open Modal window for assigning and revoking permissions
-   * @param {object} role Active role to be used
+   * @param {object} item Active role to be used
    * @version 0.1.0
    * @since 0.1.0
    */
