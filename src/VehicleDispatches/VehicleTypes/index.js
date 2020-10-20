ff08371fa434eccd09700b0c178893a78db756e0
Benson Maruchu <benmaruchu@gmail.com>
2020-10-20 18:24:44 +0300

    refactor(vehicle-types): migrate to react hooks

--- src/VehicleDispatches/VehicleTypes/index.js
+++ src/VehicleDispatches/VehicleTypes/index.js
@@ -1,15 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { httpActions } from '@codetanzania/ewea-api-client';
-import { Connect, reduxActions } from '@codetanzania/ewea-api-states';
+import { Connect } from '@codetanzania/ewea-api-states';
 import { Modal, Col } from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
-import isArray from 'lodash/isArray';
 import get from 'lodash/get';
 import Topbar from '../../components/Topbar';
 import SettingForm from '../../components/SettingForm';
 import NotificationForm from '../../components/NotificationForm';
-import { notifyError, notifySuccess } from '../../util';
+import { useList } from '../../hooks';
 import ItemList from '../../components/List';
 import ListItem from '../../components/ListItem';
 
@@ -22,22 +21,7 @@ const {
   getRoles,
   getVehicleTypesExportUrl,
 } = httpActions;
-/* redux actions */
-const {
-  getVehicleTypes,
-  openVehicleTypeForm,
-  searchVehicleTypes,
-  selectVehicleType,
-  closeVehicleTypeForm,
-  paginateVehicleTypes,
-  refreshVehicleTypes,
-  deleteVehicleType,
-  postVehicleType,
-  putVehicleType,
-} = reduxActions;
 
-/* ui */
-const { confirm } = Modal;
 /* constants */
 const nameSpan = { xxl: 5, xl: 5, lg: 5, md: 5, sm: 16, xs: 14 };
 const codeSpan = { xxl: 3, xl: 3, lg: 3, md: 3, sm: 4, xs: 4 };
@@ -47,341 +31,199 @@ const headerLayout = [
   { ...codeSpan, header: 'Code' },
   { ...descriptionSpan, header: 'Description' },
 ];
+const FIELDS_TO_SHARE = {
+  name: { header: 'Name', dataIndex: 'strings.name.en', defaultValue: 'N/A' },
+  description: {
+    header: 'Description',
+    dataIndex: 'strings.description.en',
+    defaultValue: 'N/A',
+  },
+};
 
 /**
- * @class
- * @name VehicleType
+ * @function
+ * @name VehicleTypes
  * @description Render Party(Agency) Ownership types list which have search box,
- *
+ * @param { object} props component properties object
+ * @param {object[]} props.vehicleTypes List of vehicle types from the API
+ * @param {object} props.vehicleType Selected vehicle type from from the API
+ * @param {boolean} props.loading Flag to indicate fetching data from API
+ * @param {boolean} props.posting Flag to indicate posting data to the API
+ * @param {number} props.page Current page
+ * @param {boolean} props.showForm Flag for controlling visibility of the form for
+ * creating or updating resource
+ * @param {string} props.searchQuery Search query string
+ * @param {number} props.total Total number of resources in the API
+ * @returns {object} Vehicle types list ui
  * @version 0.1.0
  * @since 0.1.0
  */
-class VehicleType extends Component {
-  // eslint-disable-next-line react/state-in-constructor
-  state = {
-    isEditForm: false,
-    showNotificationForm: false,
-    notificationBody: undefined,
-  };
+const VehicleTypes = ({
+  vehicleTypes,
+  loading,
+  page,
+  posting,
+  vehicleType,
+  showForm,
+  searchQuery,
+  total,
+}) => {
+  const {
+    isEditForm,
+    showNotificationForm,
+    notificationBody,
 
-  componentDidMount() {
-    getVehicleTypes();
-  }
+    handleOnOpenForm,
+    handleOnCloseForm,
+    handleOnSearch,
+    handleOnEdit,
+    handleOnCloseNotificationForm,
+    handleAfterCloseForm,
+    handleAfterCloseNotificationForm,
+    handleOnRefreshList,
+    handleOnArchiveItem,
+    handleOnCreateItem,
+    handleOnUpdateItem,
+    handleOnShare,
+    handleOnPaginate,
+  } = useList('vehicleTypes');
 
-  /**
-   * @function
-   * @name openVehicleTypesForm
-   * @description Open vehicle type form
-   *
-   * @version 0.1.0
-   * @since 0.1.0
-   */
-  openVehicleTypesForm = () => {
-    openVehicleTypeForm();
-  };
+  return (
+    <>
+      {/* Topbar */}
+      <Topbar
+        search={{
+          size: 'large',
+          placeholder: 'Search for vehicle types here ...',
+          onChange: handleOnSearch,
+          value: searchQuery,
+        }}
+        action={{
+          label: 'New Type',
+          icon: <PlusOutlined />,
+          size: 'large',
+          title: 'Add New Vehicle Type',
+          onClick: handleOnOpenForm,
+        }}
+      />
+      {/* end Topbar */}
 
-  /**
-   * @function
-   * @name closeVehicleTypeForm
-   * @description close vehicle type form
-   *
-   * @version 0.1.0
-   * @since 0.1.0
-   */
-  closeVehicleTypeForm = () => {
-    closeVehicleTypeForm();
-    this.setState({ isEditForm: false });
-  };
-
-  /**
-   * @function
-   * @name searchVehicleTypes
-   * @description Search Vehicle types List based on supplied filter word
-   *
-   * @param {object} event Event instance
-   *
-   * @version 0.1.0
-   * @since 0.1.0
-   */
-  searchVehicleTypes = (event) => {
-    searchVehicleTypes(event.target.value);
-  };
-
-  /**
-   * @function
-   * @name handleEdit
-   * @description Handle on Edit action for list item
-   *
-   * @param {object} vehicleType vehicle type to be edited
-   *
-   * @version 0.1.0
-   * @since 0.1.0
-   */
-  handleEdit = (vehicleType) => {
-    selectVehicleType(vehicleType);
-    this.setState({ isEditForm: true });
-    openVehicleTypeForm();
-  };
-
-  /**
-   * @function
-   * @name handleAfterCloseForm
-   * @description Perform post close form cleanups
-   * @version 0.1.0
-   * @since 0.1.0
-   */
-  handleAfterCloseForm = () => {
-    selectVehicleType(null);
-    this.setState({ isEditForm: false });
-  };
-
-  /**
-   * @function
-   * @name handleRefreshVehicleType
-   * @description Handle list refresh action
-   * @version 0.1.0
-   * @since 0.1.0
-   */
-  handleRefreshVehicleType = () => {
-    refreshVehicleTypes(
-      () => {
-        notifySuccess('Vehicle types were refreshed successfully');
-      },
-      () => {
-        notifyError(
-          'An Error occurred while refreshing Vehicle types please contact system administrator'
-        );
-      }
-    );
-  };
-
-  /**
-   * @function
-   * @name showArchiveConfirm
-   * @description show confirm modal before archiving a vehicle type
-   * @param {object} item vehicleType to archive
-   * @version 0.1.0
-   * @since 0.1.0
-   */
-  showArchiveConfirm = (item) => {
-    confirm({
-      title: `Are you sure you want to archive ${item.strings.name.en} ?`,
-      okText: 'Yes',
-      okType: 'danger',
-      cancelText: 'No',
-      onOk() {
-        return new Promise((resolve) => {
-          deleteVehicleType(
-            item._id, // eslint-disable-line
-            () => {
-              resolve();
-              notifySuccess('Vehicle Type was archived successfully');
-            },
-            () => {
-              resolve();
-              notifyError(
-                'An error occurred while archiving Vehicle Type, Please contact your system Administrator'
-              );
+      {/* list starts */}
+      <ItemList
+        itemName="Vehicle Type"
+        items={vehicleTypes}
+        page={page}
+        itemCount={total}
+        loading={loading}
+        // onFilter={this.openFiltersModal}
+        onShare={(items) => handleOnShare(items, FIELDS_TO_SHARE)}
+        onRefresh={handleOnRefreshList}
+        onPaginate={handleOnPaginate}
+        generateExportUrl={getVehicleTypesExportUrl}
+        headerLayout={headerLayout}
+        renderListItem={({
+          item,
+          isSelected,
+          onSelectItem,
+          onDeselectItem,
+        }) => (
+          <ListItem
+            key={item._id} // eslint-disable-line
+            item={item}
+            name={item.strings.name.en}
+            isSelected={isSelected}
+            avatarBackgroundColor={item.strings.color}
+            onSelectItem={onSelectItem}
+            onDeselectItem={onDeselectItem}
+            title={
+              <span className="text-sm">
+                {get(item, 'strings.name.en', 'N/A')}
+              </span>
             }
-          );
-        });
-      },
-    });
-  };
+            secondaryText={
+              <span className="text-xs">
+                {get(item, 'strings.code', 'N/A')}
+              </span>
+            }
+            actions={[
+              {
+                name: 'Edit Vehicle Types',
+                title: 'Update Vehicle Types Details',
+                onClick: () => handleOnEdit(item),
+                icon: 'edit',
+              },
+              {
+                name: 'Share Vehicle Types',
+                title: 'Share Vehicle Types details with others',
+                onClick: () => handleOnShare(item, FIELDS_TO_SHARE),
+                icon: 'share',
+              },
+              {
+                name: 'Archive Vehicle Types',
+                title: 'Remove Vehicle Types from list of active vehicle types',
+                onClick: () => handleOnArchiveItem(item),
+                icon: 'archive',
+              },
+            ]}
+          >
+            {/* eslint-disable react/jsx-props-no-spreading */}
+            <Col {...nameSpan}>{item.strings.name.en}</Col>
+            <Col {...codeSpan}>{item.strings.code}</Col>
+            <Col {...descriptionSpan}>{item.strings.description.en}</Col>
+            {/* eslint-enable react/jsx-props-no-spreading */}
+          </ListItem>
+        )}
+      />
+      {/* end list */}
 
-  /**
-   * @function
-   * @name handleShare
-   * @description Handle share multiple event Indicators
-   * @param {object[]| object} vehicleTypes event Indicators list to be shared
-   * @version 0.1.0
-   * @since 0.1.0
-   */
-  handleShare = (vehicleTypes) => {
-    let message = '';
-    if (isArray(vehicleTypes)) {
-      const vehicleTypeList = vehicleTypes.map(
-        (vehicleType) =>
-          `Name: ${vehicleType.strings.name.en}\nDescription: ${
-            // eslint-disable-line
-            vehicleType.strings.description.en
-          }\n`
-      );
-
-      message = vehicleTypeList.join('\n\n\n');
-    } else {
-      message = `Name: ${vehicleTypes.strings.name.en}\nDescription: ${
-        // eslint-disable-line
-        vehicleTypes.strings.description.en
-      }\n`;
-    }
-
-    this.setState({ notificationBody: message, showNotificationForm: true });
-  };
-
-  /**
-   * @function
-   * @name closeNotificationForm
-   * @description Handle on notify vehicleTypes
-   * @version 0.1.0
-   * @since 0.1.0
-   */
-  closeNotificationForm = () => {
-    this.setState({ showNotificationForm: false });
-  };
-
-  render() {
-    const {
-      vehicleTypes,
-      loading,
-      page,
-      posting,
-      vehicleType,
-      showForm,
-      searchQuery,
-      total,
-    } = this.props;
-    const { isEditForm, showNotificationForm, notificationBody } = this.state;
-
-    return (
-      <>
-        {/* Topbar */}
-        <Topbar
-          search={{
-            size: 'large',
-            placeholder: 'Search for vehicle types here ...',
-            onChange: this.searchVehicleTypes,
-            value: searchQuery,
-          }}
-          action={{
-            label: 'New Type',
-            icon: <PlusOutlined />,
-            size: 'large',
-            title: 'Add New Vehicle Type',
-            onClick: this.openVehicleTypesForm,
-          }}
+      {/* Vehicle Type modal */}
+      <Modal
+        title="Notify Vehicle Type"
+        visible={showNotificationForm}
+        onCancel={handleOnCloseNotificationForm}
+        footer={null}
+        destroyOnClose
+        maskClosable={false}
+        className="modal-window-50"
+        afterClose={handleAfterCloseNotificationForm}
+      >
+        <NotificationForm
+          onSearchRecipients={getFocalPeople}
+          onSearchJurisdictions={getJurisdictions}
+          onSearchGroups={getPartyGroups}
+          onSearchAgencies={getAgencies}
+          onSearchRoles={getRoles}
+          body={notificationBody}
+          onCancel={handleOnCloseNotificationForm}
         />
-        {/* end Topbar */}
+      </Modal>
+      {/* end Vehicle Type modal */}
 
-        {/* list starts */}
-        <ItemList
-          itemName="Vehicle Type"
-          items={vehicleTypes}
-          page={page}
-          itemCount={total}
-          loading={loading}
-          // onFilter={this.openFiltersModal}
-          onNotify={this.openNotificationForm}
-          onShare={this.handleShare}
-          onRefresh={this.handleRefreshVehicleType}
-          onPaginate={(nextPage) => paginateVehicleTypes(nextPage)}
-          generateExportUrl={getVehicleTypesExportUrl}
-          headerLayout={headerLayout}
-          renderListItem={({
-            item,
-            isSelected,
-            onSelectItem,
-            onDeselectItem,
-          }) => (
-            <ListItem
-              key={item._id} // eslint-disable-line
-              item={item}
-              name={item.strings.name.en}
-              isSelected={isSelected}
-              avatarBackgroundColor={item.strings.color}
-              onSelectItem={onSelectItem}
-              onDeselectItem={onDeselectItem}
-              title={
-                <span className="text-sm">
-                  {get(item, 'strings.name.en', 'N/A')}
-                </span>
-              }
-              secondaryText={
-                <span className="text-xs">
-                  {get(item, 'strings.code', 'N/A')}
-                </span>
-              }
-              actions={[
-                {
-                  name: 'Edit Vehicle Types',
-                  title: 'Update Vehicle Types Details',
-                  onClick: () => this.handleEdit(item),
-                  icon: 'edit',
-                },
-                {
-                  name: 'Share Vehicle Types',
-                  title: 'Share Vehicle Types details with others',
-                  onClick: () => this.handleShare(item),
-                  icon: 'share',
-                },
-                {
-                  name: 'Archive Vehicle Types',
-                  title:
-                    'Remove Vehicle Types from list of active vehicle types',
-                  onClick: () => this.showArchiveConfirm(item),
-                  icon: 'archive',
-                },
-              ]}
-            >
-              {/* eslint-disable react/jsx-props-no-spreading */}
-              <Col {...nameSpan}>{item.strings.name.en}</Col>
-              <Col {...codeSpan}>{item.strings.code}</Col>
-              <Col {...descriptionSpan}>{item.strings.description.en}</Col>
-              {/* eslint-enable react/jsx-props-no-spreading */}
-            </ListItem>
-          )}
+      {/* create/edit form modal */}
+      <Modal
+        title={isEditForm ? 'Edit Vehicle Type' : 'Add New Vehicle Type'}
+        visible={showForm}
+        className="modal-window-50"
+        footer={null}
+        onCancel={handleOnCloseForm}
+        afterClose={handleAfterCloseForm}
+        maskClosable={false}
+        destroyOnClose
+      >
+        <SettingForm
+          setting={vehicleType}
+          posting={posting}
+          onCancel={handleOnCloseForm}
+          onCreate={handleOnCreateItem}
+          onUpdate={handleOnUpdateItem}
         />
-        {/* end list */}
+      </Modal>
+      {/* end create/edit form modal */}
+    </>
+  );
+};
 
-        {/* Vehicle Type modal */}
-        <Modal
-          title="Notify Vehicle Type"
-          visible={showNotificationForm}
-          onCancel={this.closeNotificationForm}
-          footer={null}
-          destroyOnClose
-          maskClosable={false}
-          className="modal-window-50"
-          afterClose={this.handleAfterCloseNotificationForm}
-        >
-          <NotificationForm
-            onSearchRecipients={getFocalPeople}
-            onSearchJurisdictions={getJurisdictions}
-            onSearchGroups={getPartyGroups}
-            onSearchAgencies={getAgencies}
-            onSearchRoles={getRoles}
-            body={notificationBody}
-            onCancel={this.closeNotificationForm}
-          />
-        </Modal>
-        {/* end Vehicle Type modal */}
-
-        {/* create/edit form modal */}
-        <Modal
-          title={isEditForm ? 'Edit Vehicle Type' : 'Add New Vehicle Type'}
-          visible={showForm}
-          className="modal-window-50"
-          footer={null}
-          onCancel={this.closeVehicleTypeForm}
-          afterClose={this.handleAfterCloseForm}
-          maskClosable={false}
-          destroyOnClose
-        >
-          <SettingForm
-            setting={vehicleType}
-            posting={posting}
-            onCancel={this.closeVehicleTypeForm}
-            onCreate={postVehicleType}
-            onUpdate={putVehicleType}
-          />
-        </Modal>
-        {/* end create/edit form modal */}
-      </>
-    );
-  }
-}
-
-VehicleType.propTypes = {
+VehicleTypes.propTypes = {
   loading: PropTypes.bool.isRequired,
   vehicleTypes: PropTypes.arrayOf(PropTypes.shape({ name: PropTypes.string }))
     .isRequired,
@@ -393,12 +235,12 @@ VehicleType.propTypes = {
   showForm: PropTypes.bool.isRequired,
 };
 
-VehicleType.defaultProps = {
+VehicleTypes.defaultProps = {
   vehicleType: null,
   searchQuery: undefined,
 };
 
-export default Connect(VehicleType, {
+export default Connect(VehicleTypes, {
   vehicleTypes: 'vehicleTypes.list',
   vehicleType: 'vehicleTypes.selected',
   loading: 'vehicleTypes.loading',
