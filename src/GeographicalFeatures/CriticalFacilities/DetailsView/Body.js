9bf01a3875b4089942227ddff16bc54bf93c6c12
mloweedgar <mloweedgar0@gmail.com>
2021-06-07 10:28:35 +0300

    feat(critical facilities details): add Body empty component for rendering critical facility details

---
+++ src/GeographicalFeatures/CriticalFacilities/DetailsView/Body.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+/**
+ * @function
+ * @name CriticalFacilityDetailsViewBody
+ * @description details of a critical facility
+ * @returns {object} React component
+ * @version 0.1.0
+ * @since 0.1.0
+ */
+const CriticalFacilityDetailsViewBody = () => {
+  return <div>This is body</div>;
+};
+
+export default CriticalFacilityDetailsViewBody;
