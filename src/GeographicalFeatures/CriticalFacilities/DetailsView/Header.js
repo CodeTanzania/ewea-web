11b7bb4fa98791526f8f780f5c983e612a4277cd
mloweedgar <mloweedgar0@gmail.com>
2021-06-07 10:29:32 +0300

    feat(critical facility details): add Header empty component for rendering critical facility details

---
+++ src/GeographicalFeatures/CriticalFacilities/DetailsView/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+/**
+ * @function
+ * @name CriticalFacilityDetailsViewHeader
+ * @description header details of a critical facility
+ * @returns {object} React component
+ * @version 0.1.0
+ * @since 0.1.0
+ */
+const CriticalFacilityDetailsViewHeader = () => {
+  return <div>This is header</div>;
+};
+
+export default CriticalFacilityDetailsViewHeader;
